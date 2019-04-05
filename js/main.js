2413d3ba60751da6a35ada6ad543fc59272cc294
ovi100 <ovi0167@hotmail.com>
2019-04-06 00:02:21 +0600

    Footer Added

--- js/main.js
+++ js/main.js
@@ -135,6 +135,43 @@ $(document).ready(function(){
 	    }
 	})
 
+	/*--------------------magnificPopup------------------*/
+	var videoPlay = $('.video-play');
+    videoPlay.magnificPopup({
+      type: 'iframe',
+      closeBtnInside: false,
+      closeOnContentClick: true,
+      tLoading: '', // remove text from preloader
+
+
+      iframe: {
+        markup: '<div class="mfp-iframe-scaler">' +
+          '<div class="mfp-close"></div>' +
+          '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+        patterns: {
+          youtube: {
+            index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+            id: 'v=', // String that splits URL in a two parts, second part should be %id%
+            // Or null - full URL will be returned
+            // Or a function that should return %id%, for example:
+            // id: function(url) { return 'parsed id'; }
+
+            src: 'https://www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+          }
+
+
+
+          // you may add here more sources
+
+        },
+
+        srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+      }
+    });
+
 });
 
 
